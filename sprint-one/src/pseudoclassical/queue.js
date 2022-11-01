c558b07b87ad0c769d7ec0dc53cda5ba20930ee0
Midori <midorili998@gmail.com>
2022-11-01 16:53:33 -0700

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -11,17 +11,21 @@ Queue.prototype.enqueue = function(value) {
     storage[0] = value;
   } else if (Object.keys(storage).length === 1) {
     var firstElement = Object.keys(storage)[0];
-    storage[firstElement + 1] = value;
+    storage[Number(firstElement) + 1] = value;
   } else {
     var lastElement = Object.keys(storage).length - 1;
-    storage[lastElement + 1] = value;
+    storage[Number(lastElement) + 1] = value;
   }
 },
 
 Queue.prototype.dequeue = function() {
-  var deleted = Object.values(storage)[0];
-  delete storage[0];
-  return deleted;
+  if (Object.keys(storage).length !== 0) {
+    var deleted = Object.values(storage)[0];
+    var deletedKey = Object.keys(storage)[0];
+    delete storage[deletedKey];
+    return deleted;
+  }
+  return '';
 },
 
 Queue.prototype.size = function() {
