66516b108cc288a6ec4d5025ac5547123cad59f5
Midori <midorili998@gmail.com>
2022-11-01 16:51:30 -0700

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -18,17 +18,21 @@ var queueMethods = {
       storage[0] = value;
     } else if (Object.keys(storage).length === 1) {
       var firstElement = Object.keys(storage)[0];
-      storage[firstElement + 1] = value;
+      storage[Number(firstElement) + 1] = value;
     } else {
       var lastElement = Object.keys(storage).length - 1;
-      storage[lastElement + 1] = value;
+      storage[Number(lastElement) + 1] = value;
     }
   },
 
   dequeue: function() {
-    var deleted = Object.values(storage)[0];
-    delete storage[0];
-    return deleted;
+    if (Object.keys(storage).length !== 0) {
+      var deleted = Object.values(storage)[0];
+      var deletedKey = Object.keys(storage)[0];
+      delete storage[deletedKey];
+      return deleted;
+    }
+    return '';
   },
 
   size: function() {
