b331c631d83361e4c236b8172055407a4b931a04
Midori <midorili998@gmail.com>
2022-11-12 09:54:16 -0800

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //check if array contains item we're trying to find
+  if (this._storage.indexOf(item) === -1) {
+    // if not we add it
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  console.log(this._storage);
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  //find item index with indexOf
+  var index = this._storage.indexOf(item);
+  if (index !== -1) {
+    //if found, this._storage.splice(index, 1)
+    this._storage.splice(index, 1);
+  }
+
 };
 
 /*
