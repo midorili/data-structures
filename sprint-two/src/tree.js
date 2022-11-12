13c07121514949f40e138eaf65f513a25eeab079
Midori <midorili998@gmail.com>
2022-11-11 17:05:40 -0800

    Complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,33 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  jQuery.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  var res = false;
+  var traverse = function(tree) {
+    tree.children.forEach(function(child) {
+      if (child.value === target) {
+        res = true;
+      }
+      if (child.children.length > 0) {
+        traverse(child);
+      }
+    });
+  };
+  traverse(this);
+  return res;
 };
-
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
